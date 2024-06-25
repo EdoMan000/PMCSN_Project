d48069c17e04d953e9b4f8ce49fc965ab4760da1
EdoMan000 <manenti000@gmail.com>
2024-06-25 17:44:53 +0200

    first commit

--- src/main/java/org/pmcsn/Main.java
+++ src/main/java/org/pmcsn/Main.java
@@ -5,5 +5,6 @@ package org.pmcsn;
 public class Main {
     public static void main(String[] args) {
         System.out.println("First commit");
+        System.out.println("First commit again");
     }
 }
